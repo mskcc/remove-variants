58ae9cacbe43f7f44d6872af55af62202987e661
Cyriac Kandoth <ckandoth@gmail.com>
2019-03-06 16:11:46 -0500

    Low memory fix for pandas; Fix test output

--- remove_variants.py
+++ remove_variants.py
@@ -2,10 +2,8 @@
 '''
 @Description : Given a MAF, consolidate events that overlap, and remove IGR events
 @Created :  05/02/2017
-@Updated : 09/12/2018
-@author : Ronak H Shah
-@author : Tim Song
-@author : Cyriac Kandoth
+@Updated : 03/06/2019
+@author : Ronak H Shah, Tim Song, Cyriac Kandoth
 
 '''
 from __future__ import division
@@ -32,7 +30,7 @@ def main():
    args = parser.parse_args()
    if(args.verbose):
        logger.info("remove_variants: Started the run for removing simple variants.")
-   (cvDF,mafDF) =read_maf(args)
+   (cvDF,mafDF) = read_maf(args)
    (posToCheck) = make_coordinate_for_complex_variants(cvDF)
    (cleanDF) = remove_variants(posToCheck,mafDF)
    write_output(args,cleanDF)
@@ -40,7 +38,7 @@ def main():
        logger.info("remove_variants: Finished the run for removing simple variants.")
 
 def read_maf(args):
-    dataDF = pd.read_table(args.inputMaf, comment="#", low_memory=False)
+    dataDF = pd.read_table(args.inputMaf, comment="#", dtype=str)
     complex_variant_dataDF = dataDF.loc[dataDF['TYPE'] == "Complex"]
     return(complex_variant_dataDF,dataDF)
 
@@ -48,11 +46,11 @@ def make_coordinate_for_complex_variants(cvDF):
     positions_to_check = pd.DataFrame(columns=["Chromosome","Start"])
     count = 0
     for index, row in cvDF.iterrows():
-        chr = row.loc['Chromosome']
-        start = row.loc['Start_Position']
-        end = row.loc['End_Position']
+        chrom = row.loc['Chromosome']
+        start = int(row.loc['Start_Position'])
+        end = int(row.loc['End_Position'])
         for i in range(start,end+1):
-            positions_to_check.loc[count,["Chromosome","Start"]]=[chr,i]
+            positions_to_check.loc[count,["Chromosome","Start"]]=[chrom,i]
             count = count + 1
     return(positions_to_check)
 
@@ -61,16 +59,16 @@ def remove_variants(positions_to_check,all_variants_df):
     drop_index = list()
     for i_index, i_row in all_variants_df.iterrows():
         m_chr = i_row.loc['Chromosome']
-        m_start = i_row.loc['Start_Position']
+        m_start = int(i_row.loc['Start_Position'])
         m_type = i_row.loc['TYPE']
-        flag = False
+        m_class = i_row.loc['Variant_Classification']
         if(m_type != "Complex"):
             specific_chr_df = positions_to_check.loc[positions_to_check['Chromosome'] == m_chr]
             if((specific_chr_df["Start"] == m_start).any()):
-                flag = True
                 drop_index.append(i_index)
+	if(m_class == "IGR"):
+            drop_index.append(i_index)
     muts.drop(muts.index[drop_index], inplace=True)
-    muts.drop(muts[muts.Variant_Classification == "IGR"].index, inplace=True)
     return(muts)
 
 def write_output(args,output_DF):
